f7688978045b20ade59aa6f9e32a138103abc32b
cjuniordev <carlosrogeriocr02@gmail.com>
2023-03-20 09:19:40 -0300

    fix: dont get content type in get method

--- src/app/Requests/Request.php
+++ src/app/Requests/Request.php
@@ -7,16 +7,20 @@ use App\Utils\Arr;
 class Request
 {
     private const CONTENT_TYPE_FORM_DATA = 'multipart/form-data';
-
+    private const CONTENT_TYPE_JSON = 'application/json';
+    private ?string $contentType;
     private array $data;
 
     public function __construct()
     {
-        $contentType = explode(';', $_SERVER['CONTENT_TYPE'])[0];
+        $contentType = $_SERVER['CONTENT_TYPE'] ?? null;
+        $this->contentType = $contentType ? explode(';', $_SERVER['CONTENT_TYPE'])[0] : null;
 
-        $this->data = $contentType === self::CONTENT_TYPE_FORM_DATA
-            ? $_POST
-            : json_decode(file_get_contents('php://input'), true);
+        $this->data = match ($this->contentType) {
+            self::CONTENT_TYPE_FORM_DATA => $_POST,
+            self::CONTENT_TYPE_JSON => json_decode(file_get_contents('php://input'), true),
+            default => [],
+        };
     }
 
     public static function getParsedUri(): array
