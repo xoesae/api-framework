d46412750682c15a40274b67db1181d894d5af87
cjuniordev <carlosrogeriocr02@gmail.com>
2023-03-21 20:27:57 -0300

    feat: class to response

---
+++ src/app/Routes/Response.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Routes;
+
+class Response
+{
+    public static function json(array $data = [], int $code = 200): void
+    {
+        http_response_code($code);
+        echo json_encode($data);
+    }
+}
