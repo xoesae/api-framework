4b16a7ec82d73c9e639c0526e3daa928aa4a1e59
cjuniordev <carlosrogeriocr02@gmail.com>
2023-03-25 14:45:44 -0300

    refactor: clean route class

--- core/Routes/Route.php
+++ core/Routes/Route.php
@@ -2,6 +2,8 @@
 
 namespace Core\Routes;
 
+use Exception;
+
 class Route
 {
     public function __construct(
@@ -10,24 +12,27 @@ class Route
         public array $params = [],
     )
     {
-        $uri = self::sanitizeUri($uri);
+        $this->uri = self::sanitizeUri($uri);
     }
 
+    /**
+     * @throws Exception
+     */
     public function action(array $params = [])
     {
         [$class, $method] = explode('@', $this->action);
         $class = 'App\\Controllers\\' . $class;
 
-        if (!(isset($class) && isset($method))) {
-            throw new \Exception("Invalid action {$this->action}");
+        if (!(isset($method))) {
+            throw new Exception("Invalid action {$this->action}");
         }
 
         if (!class_exists($class)) {
-            throw new \Exception("Class {$class} not found");
+            throw new Exception("Class {$class} not found");
         }
 
         if (!method_exists($class, $method)) {
-            throw new \Exception("Method $this->action not found");
+            throw new Exception("Method $this->action not found");
         }
 
         $object = new $class();
@@ -39,8 +44,8 @@ class Route
         return count($this->params) > 0;
     }
 
-    public static function sanitizeUri(string $uri)
+    public static function sanitizeUri(string $uri): string
     {
-        return (substr($uri, 0, 1) === '/') ? substr($uri, 1) : $uri;
+        return (str_starts_with($uri, '/')) ? substr($uri, 1) : $uri;
     }
 }
